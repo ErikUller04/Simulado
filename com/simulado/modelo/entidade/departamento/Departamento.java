0fc3c091c8b37b39b8e647d63362da433e941754
Erik <122393715+ErikUller04@users.noreply.github.com>
2023-06-01 09:44:23 -0300

    modificando departamento

--- com/simulado/modelo/entidade/departamento/Departamento.java
+++ com/simulado/modelo/entidade/departamento/Departamento.java
@@ -1,9 +1,13 @@
 package com.simulado.modelo.entidade.departamento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.simulado.modelo.entidade.empregado.Empregado;
 
 public class Departamento {
 
+	private List<Departamento> departamentos = new ArrayList<>();
 	private String nome;
 	private Empregado[] empregado;
 	private int quantidadeEmpregados;
@@ -33,6 +37,20 @@ public class Departamento {
 		this.empregado = empregado;
 	}
 	
+	
+	
+	public List<Departamento> getDepartamentos() {
+		return departamentos;
+	}
+
+	public void setDepartamentos(List<Departamento> departamentos) {
+		this.departamentos = departamentos;
+	}
+
+	public Departamento cadastrarDepartamento (Departamento departamento) {
+		
+		
+	}
 	
 	
 	
